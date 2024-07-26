5395073e5abab8178640ca726a59e33c92fa7a0b
Marius Storhaug <marstor@hotmail.com>
2024-07-26 13:59:31 +0200

    Initial version (#1)
    ## Description
    
    - Initial version
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,1 +1,84 @@
-Write-Host "Hello, $SUBJECT!"
+[CmdletBinding()]
+param()
+
+$n = [System.Environment]::NewLine
+
+
+$CONTEXT_GITHUB = $env:CONTEXT_GITHUB | ConvertFrom-Json -Depth 100
+'::group::Context: [GITHUB]'
+$CONTEXT_GITHUB | ConvertTo-Json -Depth 100
+
+'::group::Context: [GITHUB_EVENT]'
+$CONTEXT_GITHUB.event | ConvertTo-Json -Depth 100
+
+'::group::Context: [GITHUB_EVENT_ENTERPRISE]'
+$CONTEXT_GITHUB | ConvertTo-Json -Depth 100
+
+'::group::Context: [GITHUB_EVENT_ORGANIZATION]'
+$CONTEXT_GITHUB.event.organization | ConvertTo-Json -Depth 100
+
+'::group::Context: [GITHUB_EVENT_REPOSITORY]'
+$CONTEXT_GITHUB.event.repository | ConvertTo-Json -Depth 100
+
+'::group::Context: [ENV]'
+$env:CONTEXT_ENV
+
+# '::group::Context: [VARS]'
+# $env:CONTEXT_VARS
+
+'::group::Context: [JOB]'
+$env:CONTEXT_JOB
+
+# '::group::Context: [JOBS]'
+# $env:CONTEXT_JOBS
+
+'::group::Context: [STEPS]'
+$env:CONTEXT_STEPS
+
+'::group::Context: [RUNNER]'
+$env:CONTEXT_RUNNER
+
+# '::group::Context: [SECRETS]'
+# $env:CONTEXT_SECRETS
+
+'::group::Context: [STRATEGY]'
+$env:CONTEXT_STRATEGY
+
+'::group::Context: [MATRIX]'
+$env:CONTEXT_MATRIX
+
+# '::group::Context: [NEEDS]'
+# $env:CONTEXT_NEEDS
+
+'::group::Context: [INPUTS]'
+$env:CONTEXT_INPUTS
+
+"::group::File system at [$pwd]"
+Get-ChildItem -Path . | Select-Object FullName | Sort-Object FullName | Format-Table -AutoSize -Wrap
+
+'::group::Environment Variables'
+Get-ChildItem env: | Where-Object {$_.Name -notlike 'CONTEXT_*'} | Sort-Object Name | Format-Table -AutoSize -Wrap
+
+"::group::PowerShell variables"
+Get-Variable | Where-Object {$_.Name -notlike 'CONTEXT_*'} | Sort-Object Name | Format-Table -AutoSize -Wrap
+
+"::group::PSVersionTable"
+$PSVersionTable | Select-Object *
+
+"::group::ExecutionContext"
+$ExecutionContext | Select-Object *
+
+"::group::Host"
+$Host | Select-Object *
+
+"::group::MyInvocation"
+$MyInvocation | Select-Object *
+
+"::group::PSCmdlet"
+$PSCmdlet | Select-Object *
+
+"::group::PSSessionOption"
+$PSSessionOption | Select-Object *
+
+"::group::PSStyle"
+$PSStyle | Select-Object *
