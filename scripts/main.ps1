15adcae966551a65b4ee48e477a15d0a539b66a5
Marius Storhaug <marstor@hotmail.com>
2024-07-26 12:35:52 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -4,10 +4,19 @@ param()
 $github = $env:CONTEXT_GITHUB | ConvertFrom-Json -Depth 100
 
 '::group::Context: [GITHUB]'
-$github | Select-Object -ExcludeProperty event
+$github | Select-Object -ExcludeProperty event | Sort-Object
 
 '::group::Context: [GITHUB_EVENT]'
 $github.event | ConvertTo-Json -Depth 100
+
+'::group::Context: [GITHUB_EVENT_ENTERPRISE]'
+$github.event.enterprise | Sort-Object
+
+'::group::Context: [GITHUB_EVENT_ORGANIZATION]'
+$github.event.organization | Sort-Object
+
+'::group::Context: [GITHUB_EVENT_REPOSITORY]'
+$github.event.repository | Sort-Object
 
 '::group::Context: [ENV]'
 $env:CONTEXT_ENV
@@ -25,22 +34,22 @@ $env:CONTEXT_JOBS
 $env:CONTEXT_STEPS
 
 '::group::Context: [RUNNER]'
-$env:CONTEXT_RUNNER
+$env:CONTEXT_RUNNER | ConvertFrom-Json | Sort-Object
 
 '::group::Context: [SECRETS]'
-$env:CONTEXT_SECRETS
+$env:CONTEXT_SECRETS | ConvertFrom-Json | Sort-Object
 
 '::group::Context: [STRATEGY]'
-$env:CONTEXT_STRATEGY
+$env:CONTEXT_STRATEGY | ConvertFrom-Json | Sort-Object
 
 '::group::Context: [MATRIX]'
-$env:CONTEXT_MATRIX
+$env:CONTEXT_MATRIX | ConvertFrom-Json | Sort-Object
 
 '::group::Context: [NEEDS]'
-$env:CONTEXT_NEEDS
+$env:CONTEXT_NEEDS | ConvertFrom-Json | Sort-Object
 
 '::group::Context: [INPUTS]'
-$env:CONTEXT_INPUTS
+$env:CONTEXT_INPUTS | ConvertFrom-Json | Sort-Object
 
 '::group::Environment Variables'
 Get-ChildItem env: | Format-Table -AutoSize
