79dc87a49b534905c9d562587ca84a4378e75961
Marius Storhaug <marstor@hotmail.com>
2024-07-26 11:59:37 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -2,43 +2,43 @@
 param()
 
 '::group::Context: [GITHUB]'
-$env:CONTEXT_GITHUB | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_GITHUB
 
 '::group::Context: [ENV]'
-$env:CONTEXT_ENV | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_ENV
 
 '::group::Context: [VARS]'
-$env:CONTEXT_VARS | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_VARS
 
 '::group::Context: [JOB]'
-$env:CONTEXT_JOB | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_JOB
 
-# '::group::Context: [JOBS]'
-# $env:CONTEXT_JOBS | ConvertFrom-Json | Format-Table -AutoSize
+'::group::Context: [JOBS]'
+$env:CONTEXT_JOBS
 
 '::group::Context: [STEPS]'
-$env:CONTEXT_STEPS | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_STEPS
 
 '::group::Context: [RUNNER]'
-$env:CONTEXT_RUNNER | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_RUNNER
 
-# '::group::Context: [SECRETS]'
-# $env:CONTEXT_SECRETS | ConvertFrom-Json | Format-Table -AutoSize
+'::group::Context: [SECRETS]'
+$env:CONTEXT_SECRETS
 
 '::group::Context: [STRATEGY]'
-$env:CONTEXT_STRATEGY | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_STRATEGY
 
 '::group::Context: [MATRIX]'
-$env:CONTEXT_MATRIX | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_MATRIX
 
-# '::group::Context: [NEEDS]'
-# $env:CONTEXT_NEEDS | ConvertFrom-Json | Format-Table -AutoSize
+'::group::Context: [NEEDS]'
+$env:CONTEXT_NEEDS
 
 '::group::Context: [INPUTS]'
-$env:CONTEXT_INPUTS | ConvertFrom-Json | Format-Table -AutoSize
+$env:CONTEXT_INPUTS
 
 '::group::Environment Variables'
-Get-ChildItem env: | Format-Table -AutoSize
+Get-ChildItem env: | Format-List
 
 '::group::File system at [$pwd]'
 Get-ChildItem -Path . | Select-Object -ExpandProperty FullName | Sort-Object
