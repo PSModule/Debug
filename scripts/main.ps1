1520d3a3ff77577e552336ff48b936e96dd70dd5
Marius Storhaug <marstor@hotmail.com>
2024-07-26 13:42:41 +0200

    test

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -6,141 +6,79 @@ $n = [System.Environment]::NewLine
 
 $CONTEXT_GITHUB = $env:CONTEXT_GITHUB | ConvertFrom-Json -Depth 100
 '::group::Context: [GITHUB]'
-$CONTEXT_GITHUB | Select-Object -ExcludeProperty event | ConvertTo-Json -Depth 100
+$CONTEXT_GITHUB | ConvertTo-Json -Depth 100
 
 '::group::Context: [GITHUB_EVENT]'
-Write-Verbose ($n + ($CONTEXT_GITHUB.event.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$CONTEXT_GITHUB.event | ConvertTo-Json -Depth 100
 
 '::group::Context: [GITHUB_EVENT_ENTERPRISE]'
-Write-Verbose ($n + ($CONTEXT_GITHUB.event.enterprise.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$CONTEXT_GITHUB | ConvertTo-Json -Depth 100
 
 '::group::Context: [GITHUB_EVENT_ORGANIZATION]'
-Write-Verbose ($n + ($CONTEXT_GITHUB.event.organization.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$CONTEXT_GITHUB.event.organization | ConvertTo-Json -Depth 100
 
 '::group::Context: [GITHUB_EVENT_REPOSITORY]'
-Write-Verbose ($n + ($CONTEXT_GITHUB.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$CONTEXT_GITHUB.event.repository | ConvertTo-Json -Depth 100
 
 '::group::Context: [ENV]'
-$CONTEXT_ENV = $env:CONTEXT_ENV | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($context_env.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_ENV
 
 # '::group::Context: [VARS]'
 # $env:CONTEXT_VARS
 
 '::group::Context: [JOB]'
-$CONTEXT_JOB = $env:CONTEXT_JOB | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_JOB.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_JOB
 
 # '::group::Context: [JOBS]'
 # $env:CONTEXT_JOBS
 
 '::group::Context: [STEPS]'
-$CONTEXT_STEPS = $env:CONTEXT_STEPS | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_STEPS.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_STEPS
 
 '::group::Context: [RUNNER]'
-$CONTEXT_RUNNER = $env:CONTEXT_RUNNER | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_RUNNER.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_RUNNER
 
 # '::group::Context: [SECRETS]'
 # $env:CONTEXT_SECRETS
 
 '::group::Context: [STRATEGY]'
-$CONTEXT_STRATEGY = $env:CONTEXT_STRATEGY | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_STRATEGY.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_STRATEGY
 
 '::group::Context: [MATRIX]'
-$CONTEXT_MATRIX = $env:CONTEXT_MATRIX | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_MATRIX.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_MATRIX
 
 # '::group::Context: [NEEDS]'
 # $env:CONTEXT_NEEDS
 
 '::group::Context: [INPUTS]'
-$CONTEXT_INPUTS = $env:CONTEXT_INPUTS | ConvertFrom-Json -Depth 100
-Write-Verbose ($n + ($CONTEXT_INPUTS.event.repository.PSObject.Properties | Foreach-Object {
-    [pscustomobject]@{
-        Name = $_.Name
-        Value = $_.Value
-    }
-} | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+$env:CONTEXT_INPUTS
 
 '::group::Environment Variables'
-Write-Verbose ($n + (Get-ChildItem env: | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+Get-ChildItem env: | Sort-Object Name | Format-Table -AutoSize -Wrap
 
 "::group::File system at [$pwd]"
-Write-Verbose ($n + (Get-ChildItem -Path . | Select-Object FullName | Sort-Object FullName | Format-Table -AutoSize -Wrap | Out-String))
+Get-ChildItem -Path . | Select-Object FullName | Sort-Object FullName | Format-Table -AutoSize -Wrap
 
 "::group::PowerShell variables"
-Write-Verbose ($n + (Get-Variable | Sort-Object Name | Format-Table -AutoSize -Wrap | Out-String))
+Get-Variable | Sort-Object Name | Format-Table -AutoSize -Wrap
 
 "::group::PSVersionTable"
-$PSVersionTable
+$PSVersionTable | Format-Table
 
 "::group::ExecutionContext"
-$ExecutionContext
+$ExecutionContext | Format-Table
 
 "::group::Host"
-$Host
+$Host | Format-Table
 
 "::group::MyInvocation"
-$MyInvocation
+$MyInvocation | Format-Table
 
 "::group::PSCmdlet"
-$PSCmdlet
+$PSCmdlet | Format-Table
 
 "::group::PSSessionOption"
-$PSSessionOption
+$PSSessionOption | Format-Table
 
 "::group::PSStyle"
-$PSStyle
+$PSStyle | Format-Table
