2b4265cf346b344c3137ea7047256bf0ac5fb691
Marius Storhaug <marstor@hotmail.com>
2025-10-04 20:39:28 +0200

    🩹 [Patch]: Add network and public IP information (#13)
    ## Description
    
    This pull request updates the `scripts/main.ps1` PowerShell script to
    enhance its network diagnostics capabilities by installing two new
    modules and adding commands to display network configuration and public
    IP information. The most important changes are grouped below:
    
    **Module installation:**
    
    * Added installation steps for the `Net` and `PublicIP` modules from the
    PowerShell Gallery to ensure required commands are available.
    
    **Network diagnostics:**
    
    * Introduced a new log group, 'Network Info', which runs
    `Get-NetIPConfiguration` to display detailed local network
    configuration.
    * Added a 'Public IP Info' log group that uses `Get-PublicIP` to display
    the machine's public IP address.

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -1,7 +1,8 @@
 [CmdletBinding()]
 param()
 
-$PSStyle.OutputRendering = 'Ansi'
+Install-PSResource -Repository PSGallery -TrustRepository -Name Net
+Install-PSResource -Repository PSGallery -TrustRepository -Name PublicIP
 Import-Module "$PSScriptRoot/Helpers.psm1"
 
 $CONTEXT_GITHUB = $env:CONTEXT_GITHUB | ConvertFrom-Json -Depth 100
@@ -68,6 +69,15 @@ LogGroup 'Context: [MATRIX]' {
 LogGroup 'Context: [INPUTS]' {
     $env:CONTEXT_INPUTS
 }
+
+LogGroup 'Network Info' {
+    Write-Host "$(Get-NetIPConfiguration | Out-String)"
+}
+
+LogGroup 'Public IP Info' {
+    Write-Host "$(Get-PublicIP | Out-String)"
+}
+
 
 LogGroup "File system at [$pwd]" {
     Get-ChildItem -Path . -Force | Select-Object -ExpandProperty FullName | Sort-Object
